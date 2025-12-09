03b675d637fb18b08ecc228bd0c307f30aab040f
nazwazhrr <nazwaazahra883@gmail.com>
2025-12-09 22:24:08 +0700

    Hapus Sosmed Dosen

--- Admin/hapus_sosmed_dosen.php
+++ Admin/hapus_sosmed_dosen.php
@@ -1,3 +1,20 @@
 <?php
 require_once '../koneksi.php';
+
+if(!isset($_GET['id'])) {
+    header("Location: kelola_sosmed_dosen.php?error=ID tidak ditemukan");
+    exit();
+}
+
+$id = pg_escape_string($conn, $_GET['id']);
+
+$query = "DELETE FROM sosmed_dosen WHERE id_sosmed_dosen = '$id'";
+
+if(pg_query($conn, $query)) {
+    header("Location: kelola_sosmed_dosen.php?success=hapus");
+    exit();
+} else {
+    header("Location: kelola_sosmed_dosen.php?error=Gagal menghapus data");
+    exit();
+}
 ?>
