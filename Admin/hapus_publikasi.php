0d993fa444e7f4a406223211b50f50734cea8856
nazwazhrr <nazwaazahra883@gmail.com>
2025-12-09 22:22:21 +0700

    Hapus Publikasi Dosen

--- Admin/hapus_publikasi.php
+++ Admin/hapus_publikasi.php
@@ -1,3 +1,20 @@
 <?php
 require_once '../koneksi.php';
+
+if(!isset($_GET['id'])) {
+    header("Location: kelola_publikasi.php?error=ID tidak ditemukan");
+    exit();
+}
+
+$id = pg_escape_string($conn, $_GET['id']);
+
+$query = "DELETE FROM publikasi_dosen WHERE id_publikasi = '$id'";
+
+if(pg_query($conn, $query)) {
+    header("Location: kelola_publikasi.php?success=hapus");
+    exit();
+} else {
+    header("Location: kelola_publikasi.php?error=Gagal menghapus data");
+    exit();
+}
 ?>
