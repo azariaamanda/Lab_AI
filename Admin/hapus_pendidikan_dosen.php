00a2fd2d187d7f0bda0b5ed2bd06ff2220bebb3f
nazwazhrr <nazwaazahra883@gmail.com>
2025-12-09 22:18:07 +0700

    Hapus Pendidikan  Dosen

--- Admin/hapus_pendidikan_dosen.php
+++ Admin/hapus_pendidikan_dosen.php
@@ -1,3 +1,25 @@
 <?php
 require_once '../koneksi.php';
+
+if(!isset($_GET['id'])) {
+    header("Location: kelola_pendidikan_dosen.php?error=ID tidak ditemukan");
+    exit();
+}
+
+// Sanitasi input (hindari SQL Injection)
+$id = pg_escape_string($conn, $_GET['id']);
+
+// Query hapus data
+$query = "DELETE FROM pendidikan_dosen WHERE id_pendidikan = '$id'";
+
+$result = pg_query($conn, $query);
+
+if($result) {
+    header("Location: kelola_pendidikan_dosen.php?success=hapus");
+    exit();
+} else {
+    $error = pg_last_error($conn);
+    header("Location: kelola_pendidikan_dosen.php?error=Gagal menghapus data: $error");
+    exit();
+}
 ?>
